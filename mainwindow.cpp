4cdbafe7a7fc1fa2eaee8798bd5cac37ef2eb94a
macksu <1115486989@qq.com>
2024-01-14 20:05:14 +0800

    天气预报项目的创建和右键退出菜单实现

---
+++ mainwindow.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setWindowFlag(Qt::FramelessWindowHint); //设置窗口无边框
+    setFixedSize(width(),height());  //设置固定窗口大小
+    //构建右键菜单
+    mExitMenu = new QMenu(this);
+    mExitAct = new QAction();
+
+    mExitAct->setText("退出");
+    mExitAct->setIcon(QIcon(":/res/close.png"));
+
+    mExitMenu->addAction(mExitAct);
+
+    connect(mExitAct,QAction::triggered,this,[=]{
+        qApp->exit(0);
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+
+}
+//重写父类的虚函数
+//父类中默认的实现，是忽略右键菜单事件  重写就可以处理右键菜单事件
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    //弹出右键菜单
+    mExitMenu->exec(QCursor::pos());
+
+    event->accept();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+ moffset = event->globalPos()-this->pos();
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+ this->move(event->globalPos()-moffset);
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+private:
+    QMenu* mExitMenu;  // 右键退出的菜单
+
+    QAction* mExitAct; //退出的行为 - 菜单项
+
+    QPoint moffset;  //窗口移动时左上角的偏移
+
+protected:
+     void contextMenuEvent(QContextMenuEvent *event);
+     void mousePressEvent(QMouseEvent *event);
+     void mouseMoveEvent(QMouseEvent *event);
+};
+#endif // MAINWINDOW_H
